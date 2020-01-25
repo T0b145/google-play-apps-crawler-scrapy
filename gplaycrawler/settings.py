3f63092e530a831971526a76fb1a97333816f451
T0b145 <tobias-braun@gmx.net>
2020-01-25 18:25:21 +0100

    Template Scraper ready
    -Dynamic Items
    - SQL Pipeline
    - MongoDB Pipeline

--- gplaycrawler/settings.py
+++ gplaycrawler/settings.py
@@ -23,17 +23,17 @@ DATABASE = "google_play_store"
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 30
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_DOMAIN = 30
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
@@ -45,7 +45,7 @@ COOKIES_ENABLED = False
 # Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'Accept-Language': 'de',
+  'Accept-Language': 'en',
 }
 
 # Enable or disable spider middlewares
@@ -74,7 +74,7 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+#AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
--- gplaycrawler/spiders/gplay.py
+++ gplaycrawler/spiders/gplay.py
@@ -11,7 +11,7 @@ import datetime
 class MySpider(CrawlSpider):
   name = "gplay"
   allowed_domains = ["play.google.com"]
-  start_urls = ["https://play.google.com/store/apps/top"]
+  start_urls = ["https://play.google.com/store/apps/"]
   rules = (Rule(LinkExtractor(allow=('/store/apps/details')),follow=True,callback='parse_link'), Rule(LinkExtractor(allow=('/store/apps',)),follow=True))
 
   def abs_url(url, response):
@@ -28,19 +28,25 @@ class MySpider(CrawlSpider):
       item = OrderedDict()
       item["Request_time"] = datetime.datetime.now()
       item["Request_url"] = response.request.url
+      #item["site_content"] = response.body
 
       item["Link"] = page.xpath('head/link[5]/@href').get()
       item["Item_name"] = page.css('h1[itemprop=name] ::text').get()#updated
       item["Developed_by"] = page.xpath("//a[@class='hrTbp R8zArc']/text()").get()
       item["Developed_by_url"] = page.xpath("//a[@class='hrTbp R8zArc']/@href").get()
-
-      item["Description"] = len(page.xpath("//meta[@itemprop='description']/@content").get())
       item["Genre"] = page.xpath('//*[@itemprop="genre"]/text()').get()
       item["Video_URL"] = page.xpath('//*[@class="MMZjL lgooh  "]/@data-trailer-url').get()
       item["Screenshots"] = page.xpath("//img[@itemprop='image']/@src").getall()
       item["LogoURL"] = page.xpath("//img[@itemprop='image']/@src").get()
       item["Warnings"]= page.xpath("//div[@class='bSIuKf']/text()").getall()
       item["Ähnliche apps"] = page.xpath("//div[@class='Ktdaqe  ']").xpath("//div[@class='uzcko']//div[@class='wXUyZd']/a/@href").getall()
+      item["Awards"] = page.xpath("//span[@class='GcFlrd']/text()").getall()
+
+      try:
+          item["Description_count"] = len(page.xpath("//meta[@itemprop='description']/@content").get())
+          #item["Description"] = page.xpath("//meta[@itemprop='description']/@content").get()
+      except:
+          item["Description_count"] = 0
 
       try:
           item["Price"] = float(page.xpath("//meta[@itemprop='price']/@content").get().split()[0].replace(",","."))
@@ -49,7 +55,7 @@ class MySpider(CrawlSpider):
 
       try:
           item["Rating_value"] = float(page.css('div[class=BHMmbe] ::text').get().replace(",","."))
-          item["Review_number"] = int(page.css('span[class="AYi5wd TBRnV"] ::text').get().replace(".",""))#updated
+          item["Review_number"] = int(page.css('span[class="AYi5wd TBRnV"] ::text').get().replace(",",""))#updated
       except AttributeError:
           item["Rating_value"] = None
           item["Review_number"] = 0
